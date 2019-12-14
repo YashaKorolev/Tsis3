fab0b52c8720c4a417e7a1a4d78f02b35f37bf0e
YashaKorolev <47750082+YashaKorolev@users.noreply.github.com>
2019-12-14 15:48:46 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 double f1(double x)
 {
-	
 	return cos(x) * tanh(x);
 }
 
@@ -20,11 +19,10 @@ double random(double min, double max)
 	double d = max - min;
 	double r = (double)rand() / RAND_MAX * d;
 	return min + r;
-	
 }
 
 
-void anneling_simulation2(double a, double b, double Tmax, double Tmin, double ALPHA, double (*f)(double)) {
+void anneling_simulation(double a, double b, double Tmax, double Tmin, double ALPHA, double (*f)(double)) {
 
 	srand(0);
 
@@ -33,43 +31,42 @@ void anneling_simulation2(double a, double b, double Tmax, double Tmin, double A
 	double x0 = random(a, b);
 	double fx0 = f(x0);
 
-	cout << "______________________________________________________________________" << endl;
-	cout << "  N |           T     |         x    |        f(x)   |          P    | " << endl;
-	cout << "______________________________________________________________________" << endl;
+	cout << "____________________________________________________________" << endl;
+	cout << "  N |      T     |    x    |   f(x)   |       P      | Chg |" << endl;
+	cout << "____________________________________________________________" << endl;
 
 	int N = 0;
 
 	while (Ti > Tmin) {
 		double x1 = random(a, b);
 		double fx1 = f(x1);
-		double PDelta = 0;
+		double PDelta = 1;
 		double delta = fx1 - fx0;
-		if (delta <= 0) {
+		bool flag = false;
 
+		if (delta <= 0) {
+			flag = true;
 			x0 = x1;
 		}
 		else {
 			PDelta = exp(-delta / Ti);
 			double p = random(0, 1);
 			if (p <= PDelta) {
+				flag = true;
 				x0 = x1;
 			}
 
 		}
 		fx0 = f(x0);
-		std::cout.precision(7);
-		
-		std::cout << ++N << setprecision(7)<<"             " << Ti  << setprecision(7) << "       " <<x0 <<  setprecision(7) << "         " << fx0 <<  setprecision(7) << "          " <<PDelta << std::endl;
-		
+
+		printf("%3i | %10.4lf | %02.5lf | % 5.5lf | %1.10lf | %3c |\n", ++N, Ti, x0, fx0, PDelta, flag ? '+' : '-');
+		flag = false;
+
 		Ti *= ALPHA;
 	}
-
-	cout << f(x0) << endl;
-
-
-
+	cout << "\n\n";
+	cout << "Minimum = f(" << x0 << ") = " << f(x0) << endl;
 }
-
 
 int main()
 {
@@ -79,11 +76,13 @@ int main()
 	double Tmin = 0.01;
 	double ALPHA = 0.95;
 	double delta = 0.1;
+	cout << "\n\nUnimodal_function_annealing_method:\n\n";
+	cout << "f(x)= cos(x) * tanh(x)" << endl << endl;
+	anneling_simulation(a, b, Tmax, Tmin, ALPHA, f1);
+	cout << "\n\n\n";
+	cout << "\n\nMultimodal_function_annealing_method:\n\n";
+	cout << "f(x)= cos(x) * tanh(x) * sin(5x)" << endl << endl;
+	anneling_simulation(a, b, Tmax, Tmin, ALPHA, f2);
 
-	anneling_simulation2(a, b, Tmax, Tmin, ALPHA, f1);
-	anneling_simulation2(a, b, Tmax, Tmin, ALPHA, f2);
-
-
-	
 	system("pause");
 }
